d3b7cb867852d4e96b5538cf7ef7dc6b5a33513c
Josh Holt <holt.josh@gmail.com>
2012-07-20 14:49:29 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+###Haibu-Control
+haibu-control is a remote control for a haibu server.
+
+You install haibu-control globally via NPM and then you create a
+"Deployfile" in your nodejs project.
+
+The format of the "Deployfile" is as follows
+
+   {
+			"user" : "joshholt",
+			"name" : "test",
+			"domain": "mytest.com",
+			"repository" : {
+				"type" : "git",
+				"url"  : "https://github.com/Marak/hellonode.git"
+			},
+			"scripts" : {
+				"start" : "server.js"
+			}
+	 }
+
+
+Once you've set up your "Deployfile" you can deploy your application to
+your haibu server with the folowing command:
+
+`haibu-control -s`
+
+You can restart your application on the haibu server with the following comand:
+
+`haibu-control -r`
+
+You can kill your application on the haibu server with the following comand:
+
+`haibu-control -k`
+
+You can completely remove your application from the haibu server with the following comand:
+
+`haibu-control -d`
---
+++ deploy.js
@@ -0,0 +1,63 @@
+#!/usr/bin/env node
+
+var eyes = require('eyes'),
+		haibu = require('haibu'),
+		optimist = require('optimist'),
+		argv  = optimist.usage('Usage: $0 -[skrd]')
+			.describe({
+				's' : 'Start application on Haibu Server, will deploy and start if this is the first time the server has seen the app',
+				'k' : 'Stop  application on Habiu Server',
+				'r' : 'Restart application on Habiu Server',
+				'd' : 'Remove all instances of application from the Haibu Server'
+			})
+			.options('s', {	alias: 'start' })
+			.options('k', { alias: 'kill' })
+			.options('r', { alias: 'restart' })
+			.options('d', { alias: 'destroy' })
+			.argv;
+
+
+var client, app, deployer;
+
+client = new haibu.drone.Client({
+	host: '127.0.0.1',
+	port: 9002
+});
+
+app = require('fs').readFileSync(require('path').join(process.pwd, '/Deployfile'));
+
+deployer = {
+	"start"   : function () {
+		client.start(app, function (err, res) { eyes.inspect(err); eyes.inspect(res); });
+	},
+	"kill"    : function () {
+		client.stop(app.name, function (err, res) { eyes.inspect(err); eyes.inspect(res); });
+	},
+	"restart" : function() {
+		client.restart(app.name, function (err, res) { eyes.inspect(err); eyes.inspect(res); });
+	},
+	"remove"  : function () {
+		client.clean(app, function (err, res) { eyes.inspect(err); eyes.inspect(res); });
+	}
+};
+
+if (!app || app === undefined) {
+	optimist.showHelp()
+	console.log("You need to create a Deployfile in the root of your project.");
+}
+
+if (argv.s || argv.start) {
+	deployer.start();
+}
+else if (argv.k || argv.kill) {
+	deployer.kill();
+}
+else if (argv.r || argv.restart) {
+	deployer.restart();
+}
+else if (argv.d || argv.destroy) {
+	deployer.remove();
+}
+else {
+	optimist.showHelp();
+}
